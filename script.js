f4a58e5ab07ed6f2e10bce0d324d0f8a07121edc
uhutruco <125451940+uhutruco@users.noreply.github.com>
2023-02-15 10:56:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let xd = document.getElementById("xd");
+let text = document.getElementById("muitosxd")
+
+let textoxd = "";
+
+xd.addEventListener("click", function() {
+    console.log("aaaaaaaaaaaaaaaaaaa");
+    textoxd += "xd ";
+    text.innerHTML = textoxd;
+})
